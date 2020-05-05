4902a6c9a24c4e7e6114587504f37611f0d82c0d
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-05-05 17:14:46 +0530

    Create PrimalityTest.java

---
+++ Placement Preparation Course/Tracks/Mathematics/PrimalityTest.java
@@ -0,0 +1,21 @@
+class Primality 
+{
+    
+    public boolean isPrime(int N)
+    {
+        if(N<2)
+        {
+            return false;
+        }
+        for(int i=2;i<=N/2;i++)
+        {
+            if(N%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
+
