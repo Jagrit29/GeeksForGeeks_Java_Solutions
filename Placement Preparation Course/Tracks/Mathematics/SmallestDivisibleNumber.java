a7f2c8e6ad43fa2d3c8f8c92978457d01a7caa63
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-05-05 13:49:52 +0530

    Update SmallestDivisibleNumber.java

--- Placement Preparation Course/Tracks/Mathematics/SmallestDivisibleNumber.java
+++ Placement Preparation Course/Tracks/Mathematics/SmallestDivisibleNumber.java
@@ -1,1 +1,32 @@
-hello.java
+//Need to find LCM of 1 to n;
+class GfG{
+    
+    //here b is greater
+    public static long gcd(long a,long b)
+    {
+        if(a==0)
+        {
+            return (long)b;
+        }
+        
+        return (long)gcd(b%a,a);  //b%a will be smaller than a only;
+    }
+	public static long getSmallestDivNum(int n){
+	    
+	    //LCM(a,b)=GCD(a,b)/a*b;
+	    long lcm=1;
+	    
+	    for(long i=1;i<=(long)n;i++)
+	    {
+	        lcm=(i*lcm)/(gcd(i,lcm));
+	    }
+	    
+	    return lcm;
+	    
+	    
+	    
+	    
+//add code here.
+
+}
+}
