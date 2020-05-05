1f81f8dbd75004abd92f3c93d5b9ff5716d4d401
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-05-05 14:59:35 +0530

    Create AbsoluteValue.java

---
+++ Placement Preparation Course/Tracks/Mathematics/AbsoluteValue.java
@@ -0,0 +1,14 @@
+class Absolute {
+    // You need to complete this function
+    public int absolute(int I) {
+        // Your code here
+        
+        //We only need to change if its neg.
+        if(I<0)
+        {
+            I=0-I;
+        }
+        
+        return I;
+    }
+}
